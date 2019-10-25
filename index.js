fdc909554afeb16a39836fadc7e4461799a1f830
imex7 <kras-imex-7@mail.ru>
2019-10-25 22:06:05 +0700

    25_10_2019

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const app = require('express')()
 const path = require('path')
+const exphbs = require('express-handlebars')
 
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.get('/', (req, res) => {
 	res.removeHeader('X-Powered-By')
 	res.setHeader('Date', new Date().toLocaleString() )
-	res.sendFile(path.join(__dirname, 'views', 'index.html'))
+	res.render('index')
+})
+
+app.get('/about', (req, res) => {
+	res.removeHeader('X-Powered-By')
+	res.setHeader('Date', new Date().toLocaleString() )
+	res.render('about')
 })
 
 const PORT = process.env.port || 3010
